36793b220f5db70e8a4f2577aafe7ac9e39a3cfa
basmazaha <82834278+basmazaha@users.noreply.github.com>
2026-03-22 02:55:12 +0200

    Update AppointmentsTable.tsx

--- app/dashboard/AppointmentsTable.tsx
+++ app/dashboard/AppointmentsTable.tsx
@@ -358,7 +358,18 @@ const handleRefresh = async () => {
       onClick={handleRefresh}
       className={`btn btn--refresh ${isRefreshing ? 'is-loading' : ''}`}
       >
-      ⟳
+      <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+       >
+       <path d="M21 12a9 9 0 1 1-3-6.7" />
+       <polyline points="21 3 21 9 15 9" />
+       </svg>
       </button>
 
           {/* زر الإضافة */}
